8e132356a9c34e46cc1ece76684157323a5066e5
FluentCoding <zSuckyLP@gmail.com>
2021-06-15 19:27:45 +0200

    test

--- src/store.ts
+++ src/store.ts
@@ -23,7 +23,7 @@ var store : GlobalStore = {
     },
     removePlayer: (client) => {
         store.players = store.players.filter(value => value.id !== client);
-        for (var key in Object.keys(store.subscriptions)) {
+        for (var key in store.subscriptions.keys()) {
             store.subscriptions[key] = store.subscriptions[key].filter(player => player.id !== client);
         }
     }
