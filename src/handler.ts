7f4bb0d8234106c3ca03932bb35141ab41a1a9de
FluentCoding <zSuckyLP@gmail.com>
2021-06-18 12:31:30 +0200

    test

--- src/handler.ts
+++ src/handler.ts
@@ -136,14 +136,18 @@ export function handleMessage(incoming: string, client: WebSocket): string {
         if(!player)
           break;
 
+        console.log("playerFound")
+
         var lobby = store.lobbies.find(lobby => lobby.players.some(player => player.id === client));
         if (!lobby)
           break;
 
+        console.log("lobbyFound")
+
         if (player.lastPosition?.x === req.x && player.lastPosition?.y === req.y && player.lastPosition?.rotation === req.rotation && player.lastPosition?.facing === (req.facing === 1))
           break; // ignore packets if last position is the same one
-
-        console.log("nice")
+        
+        console.log("newPosition")
 
         if (!player.lastPosition)
           player.lastPosition = new LastPosition(req.x, req.y, req.rotation, req.facing === 1); // fuck javascript btw
